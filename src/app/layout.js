7ff47ebbc03323caf5988c7c08f18fa123a63171
Mag1c54 <Mag1c54@yandex.ru>
2025-03-15 10:29:22 +0300

    header, hero

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,10 @@
-
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="test">
+       
         {children}
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,8 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
+import Header from "./(header)/page";
 export default function Home() {
   return (
     <div>
+      <Header />
       <h1>Hello World</h1>
     </div>
   );
